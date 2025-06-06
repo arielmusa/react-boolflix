96d03f56063ffce4afa09581fb6be6f6f683d0db
arielmusa <arielmusainfo@gmail.com>
2025-06-06 18:33:54 +0200

    feat: use stars to display vote in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as faSolidStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faRegularStar } from "@fortawesome/free-regular-svg-icons";
+
 export default function Card({ data }) {
+  const stars = [];
+  for (let i = 0; i < 5; i++) {
+    i <= data.vote
+      ? stars.push(
+          <span>
+            <FontAwesomeIcon icon={faSolidStar} key={i} />
+          </span>
+        )
+      : stars.push(
+          <span>
+            <FontAwesomeIcon icon={faRegularStar} key={i} />
+          </span>
+        );
+  }
+
   return (
     <div className="col-md-4">
       <div>
@@ -16,7 +35,7 @@ export default function Card({ data }) {
             {data.title},<strong>Titolo originale: </strong>
             {data.original_title}, <strong>Lingua originale: </strong>
             {data.language},<strong>Voto: </strong>
-            {data.vote}
+            {stars}
           </li>
         </ul>
       </div>
